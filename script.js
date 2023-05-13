8e50bf2e76cc2f517df48e9734e990e6b5652c4d
Aarav <aaravuno8@gmail.com>
2023-05-12 21:10:47 -0600

    first

---
+++ script.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfg1rE558zPO1QMQxvz7r1nqZ_S8O5gpc",
+  authDomain: "filesharingapp-2d543.firebaseapp.com",
+  databaseURL: "https://filesharingapp-2d543-default-rtdb.firebaseio.com",
+  projectId: "filesharingapp-2d543",
+  storageBucket: "filesharingapp-2d543.appspot.com",
+  messagingSenderId: "1072033781170",
+  appId: "1:1072033781170:web:a7be5173e300846ab680ab",
+  measurementId: "G-85HRJ404PF"
+};
+
+const app = initializeApp(firebaseConfig);
